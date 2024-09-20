d0a6404a7cbb58b06d74cf95397ec05b2af855e1
Mark Neider <8540826+mneider12@users.noreply.github.com>
2024-09-20 12:10:18 -0700

    Compile with gradle

---
+++ src/models/atom/Symbol.kt
@@ -0,0 +1,7 @@
+package models.atom
+
+enum class Symbol(val value: String) {
+    H("H"), HE("He"), LI("Li"), BE("Be"), B("B"), C("C"), N("N"),
+    O("O"), F("F"), NE("Ne"), NA("Na"), MG("Mg"), AL("Al"), SI("Si"),
+    P("P"), S("S"), CL("Cl"), AR("Ar"),
+}
